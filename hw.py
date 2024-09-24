6903ad93dea1a81cf48c72b444e385d65912626c
VPlaksin <valeriy905@gmail.com>
2024-09-24 19:01:16 +0300

    have done 1st hm

--- hw.py
+++ hw.py
@@ -1,6 +1,18 @@
 # lesson 1.
-print("hello")
+# task 1.2: Вывести строку "Hello world!" (без кавычек) в консоль.
+#print("hello world")
 
-for i in range(10):
+# Task 1.2.
+# name = input("vvedite imya ")
+# print("Hi, my name is ", name)
 
-NEW NEW COMMIT
+#Task 1.3:
+# name = input("vvedite imya ")
+# surname = input("vvedite surname ")
+# print("hi, my name is ", name.title(), surname.title())
+
+#task 1.4:
+a = int(input("vvedite a:__"))
+b = int(input("vvedite b:__"))
+print("SUM=", a+b)
+
